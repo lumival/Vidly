60fb05455897485b2b15502dabcb391ceaa658e6
Lumiere Valentine <lumiere.valentine@outlook.com>
2021-10-17 14:27:47 -0400

    Add project files.

---
+++ Vidly/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Vidly
+{
+    public class BundleConfig
+    {
+        // This is where we define various bundles of client-side assets 
+        // We can combine and compress multiple JavaScript or CSS files into a bundle.
+        // This reduces the number of HTTP requests required to get these assets when a page is loaded.
+        // This results in a faster page load.
+
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    // This class is a plain old CLR object or POCO, which represents the state and behavior of our application
+    // in terms of its problem domain. 
+    // It doesn't have any behavior or logic, it just has properties which we use for representing state
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        // /movies/random
+    }
+}
